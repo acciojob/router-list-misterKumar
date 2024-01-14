60df8db0a7999afb9e229cb0f134a3d3156b3372
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-14 10:53:54 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
+
 import React from "react";
 import './../styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemList from './ItemList';
-import ItemDetail from './ItemDetail';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ItemList />} />
-        <Route path="/items/:id" element={<ItemDetail />} />
-      </Routes>
+      <div>
+        {/* Do not remove the main div */}
+        <Routes>
+          <Route path="/" element={<div>
+            <h1>Item List</h1>
+            <ul>
+              <li><Link to={"/items/1"} >Item 1</Link> </li>
+              <li><Link to={"/items/2"} >Item 2</Link> </li>
+              <li><Link to={"/items/3"} >Item 3</Link> </li>
+            </ul>
+          </div>} />
+          <Route path="/items/1" element={<div><h1>Item 1</h1><p>Description for Item 1</p></div>} />
+          <Route path="/items/2" element={<div><h1>Item 2</h1><p>Description for Item 2</p></div>} />
+          <Route path="/items/3" element={<div><h1>Item 3</h1><p>Description for Item 3</p></div>} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
